e5a26df14946e1d8acf80065428566e0e6d43078
Hami <23hamiid@gmail.com>
2025-03-01 21:09:48 +0530

    refactor: restructure backend codebase and enhance error handling

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("ERROR ", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB Connection Failed: ", err);
+  });
