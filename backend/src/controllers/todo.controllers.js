8577dc87e84745fa2250f71f8d8a3b1a4c2d89cc
Hami <23hamiid@gmail.com>
2025-03-03 21:26:15 +0530

    feat: add update functionality for todos with validation

--- backend/src/controllers/todo.controllers.js
+++ backend/src/controllers/todo.controllers.js
@@ -36,4 +36,58 @@ const createTodo = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdTodo, "Todo created successfully"));
 });
 
-export { createTodo };
+const updateTodo = asyncHandler(async (req, res) => {
+  const { todoId } = req.params;
+  const { title, description, dueDate } = req.body;
+
+  if(!todoId) {
+
+  }
+
+  const todo = await Todo.findOne({
+    _id: todoId
+  });
+
+  if(title) {
+    const validTitle = titleType.safeParse(title);
+
+    if(!validTitle.success) {
+      throw new ApiError(400, "Invalid input for the title");
+    };
+  
+    todo.title = title;
+  };
+
+  if(description) {
+    const validDesc = descType.safeParse(description);
+    
+    if(!validDesc.success) {
+      throw new ApiError(400, "Invalid input type for the description");
+    };
+      
+    todo.description = description;
+  };
+
+  if(dueDate) {
+    const validDate = dueDateType.safeParse(dueDate);
+
+    if(!validDate.success) {
+      throw new ApiError(400, "Invalid input type for the date");
+    };
+    
+    todo.dueDate = dueDate;
+  };
+
+  await todo.save({validateBeforeSave: false});
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, todo, "Todo updated successfully")
+  );
+});
+
+export { 
+  createTodo,
+  updateTodo
+};
--- backend/src/routes/todo.routes.js
+++ backend/src/routes/todo.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createTodo } from "../controllers/todo.controllers.js";
+import { createTodo, updateTodo } from "../controllers/todo.controllers.js";
 
 const router = Router();
 
 router.route("/").post(createTodo);
+router.route("/update/:todoId").patch(updateTodo);
 
 export default router;
