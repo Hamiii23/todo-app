5f6619e8309be94993515ea2b95e103e9fdf30f7
Hami <23hamiid@gmail.com>
2025-03-29 21:52:05 +0530

    feat: add Sidebar component and ChangePassword page; update routing in App component and enhance SignIn page with password visibility toggle

---
+++ frontend/src/pages/ChangePassword.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Heading from "../components/Heading.jsx";
+import SubHeading from "../components/SubHeading.jsx";
+import Card from "../components/Card.jsx";
+import InputBox from "../components/InputBox.jsx";
+import Button from "../components/Button.jsx";
+
+export default function Home() {
+  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/v1/user/", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        if (res.data) {
+          setIsAuthenticated(true);
+        }
+      })
+      .catch(() => {
+        navigate("/signin");
+      });
+  }, []);
+
+  const changePasswordRequest = async () => {
+    try {
+      const res = await axios.patch(
+        "http://localhost:8000/api/v1/user/change-password",
+        {
+          oldPassword: currentPassword,
+          newPassword,
+          confirmNewPassword,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center h-screen items-center">
+      <Card>
+        <Heading label={"Change Your Password"} />
+        <SubHeading label={"Stay Organized – Add a New Task"} />
+        <InputBox
+          onChange={(e) => {
+            setCurrentPassword(e.target.value);
+          }}
+          label={"Current Password"}
+          placeholder={"Enter your current password"}
+          type={"password"}
+        />
+        <InputBox
+          onChange={(e) => {
+            setNewPassword(e.target.value);
+          }}
+          label={"New Password"}
+          placeholder={"Enter your new password"}
+          type={"password"}
+        />
+        <InputBox
+          onChange={(e) => {
+            setConfirmNewPassword(e.target.value);
+          }}
+          label={"Confirm New Password"}
+          placeholder={"confirmNewPasswordrm your new password"}
+          type={"password"}
+        />
+        <Button onClick={changePasswordRequest} label={"Change Password"} />
+      </Card>
+    </div>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -40,7 +40,7 @@ export default function Home() {
                   <h2 className="text-xl">{todo.title}</h2>
                   <div className="flex">
                   {/* {todo.description && <p>{todo.description}</p>} */}
-                  {todo.dueDate && <p> {new Date(todo.dueDate).toLocaleDateString()}</p>}
+                  {/* {todo.dueDate && <p> {new Date(todo.dueDate).toLocaleDateString()}</p>} */}
                   </div>
                   </div>
               </div>
@@ -48,14 +48,14 @@ export default function Home() {
             </div>
             <div className="flex sticky bottom-2 justify-center z-50">
                 {!isAddingTodo ? (
-                    <div
-                        onClick={() => setIsAddingTodo(true)}
-                        className="h-16 w-16 bg-blue-500 rounded-full cursor-pointer hover:bg-blue-600 flex justify-center items-center text-white shadow-lg"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                        </svg>
-                    </div>
+                      <div
+                          onClick={() => setIsAddingTodo(true)}
+                          className="h-16 w-16 bg-blue-500 rounded-full cursor-pointer hover:bg-blue-600 flex justify-center items-center text-white shadow-lg"
+                      >
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                          </svg>
+                      </div>
                 ) : (
                       <AddTodo className="z-50" />
                 )}
